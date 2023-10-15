aedf66b43966f21aeec9bacc0c65722baa69d9a8
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-15 17:26:50 +0900

    GUIデバッグ機能作成

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./controls/OrbitControls.js";
 import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.15/+esm";
+
+//UIデバッグ
+const gui = new GUI();
