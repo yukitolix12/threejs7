919fd69248c9c184aec937c8242272883dbb770c
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-16 15:53:07 +0900

    particleに色をつける

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ const count = 1000;
 
 const positionArray = new Float32Array(count * 3);
 
+
 for(let i=0; i < count * 3; i++){
     positionArray[i] = (Math.random() - 0.5) * 10;
 }
@@ -54,6 +55,7 @@ particlesGeometry.setAttribute(
 //マテリアル
 const pointsMaterial = new THREE.PointsMaterial({
     size: 0.03,
+    color: "pink",
 });
 
 //メッシュ化
